0f99fe167f04a87402d44a5cbfb1afc28635b222
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-13 19:56:20 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect "/tweets"
     else
-      erb :'/users/error'
+      redirect "/signup"
     end
   end
 
