df700b60e01300a0db189208fb30af1256149469
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-13 19:32:56 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    binding.pry
     @user = User.find_by(username: params[:username], password: params[:password])
     if @user
       session[:user_id] = @user.id
